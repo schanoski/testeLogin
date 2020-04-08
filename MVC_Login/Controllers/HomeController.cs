5502de97b03c4e0013e21ebc0d832d5dea73eacb
William Schanoski <schanoski97@gmail.com>
2020-04-07 23:04:44 -0300

    login

---
+++ MVC_Login/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using MVC_Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["usuarioLogadoID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Usuario u)
+        {
+            // esta action trata o post (login)
+            if (ModelState.IsValid) //verifica se é válido
+            {
+                using (CadastroEntities dc = new CadastroEntities())
+                {
+                    var v = dc.Usuarios.Where(a => a.NomeUsuario.Equals(u.NomeUsuario) && a.Senha.Equals(u.Senha)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["usuarioLogadoID"] = v.Id.ToString();
+                        Session["nomeUsuarioLogado"] = v.NomeUsuario.ToString();
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View(u);
+        }
+    }
+}
